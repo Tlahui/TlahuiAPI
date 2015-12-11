fdca6e6a7cd48acfc7f624d3157182fdc8e589c6
TRON <osvaldocube@gmail.com>
2015-12-11 02:20:52 -0600

    Definición de rutas para accesar al API, generación de controladores y modelos de Product y ProductImage, Configuración de la base de datos, y autoload del mismo.

---
+++ application/models/ProductImageModel.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class ProductImageModel extends CI_Model {
+
+    /**
+     * Modelo definido para interactuar con la tabla "ProductImage"
+     *
+     */
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+}
---
+++ application/models/ProductModel.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class ProductModel extends CI_Model {
+
+    /**
+     * Modelo definido para interactuar con la tabla "Product"
+     *
+     */
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+}
