860d7b94d1a57bf94e295b3c7d12c0571495125d
ethush <ethush@gmail.com>
2015-12-14 19:07:10 -0600

    new branch causen by issues on integration, added #63 #64 #65 #66 issues

---
+++ application/controllers/PurchaseController.php
@@ -0,0 +1,82 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class PurchaseController extends CI_Controller {
+
+
+  /*
+   * Función para obtener información de compra, esta recibe el idPurchase
+   * Devuelve un array con informacón y detalles de compra.
+  */
+    public function get($idPurchase) {
+        //debug mode
+        //$idPurchase = $this->input->post("idPurchase");
+
+        $response["responseStatus"] = "FAIL";
+        $response["message"] = "No existe";
+
+        if(is_numeric($idPurchase) && $idPurchase != 0 ){
+            $this->load->model("PurchaseModel");
+            $purchaseGet = $this->PurchaseModel->getInfo($idPurchase);
+            if($purchaseGet["idPurchase"] != null){
+                $this->output->set_content_type('application/json')->set_output(json_encode( $purchaseGet ));
+            }
+            else {
+                $this->output->set_content_type('application/json')->set_output(json_encode( $response ));
+            }
+        }
+        else {
+            $this->output->set_content_type('application/json')->set_output(json_encode( $response ));
+        }
+
+    }
+
+    /*
+     * Funcion para eliminar una compra, recibe idPurchase
+     * y elimina el registro validando que solicitudCancelacion=1
+     * esta funcion es usada por un usuario administrador
+     */
+    public function cancel() {
+        $idPurchase = $this->input->post("idPurchase");
+
+        $response["statusResponse"] = false;
+
+        $this->load->model("PurchaseModel");
+        $canceled = $this->PurchaseModel->cancel($idPurchase);
+
+        if($canceled) {
+            $response["responseStatus"] = "Ok";
+            $response["message"] = "Compra cancelada correctamente";
+        }
+        else {
+            $response["responseStatus"] = "FAIL";
+            $response["message"] = "La compra no pudo ser cancelada";
+        }
+
+        $this->output->set_content_type('application/json')->set_output(json_encode($response));
+    }
+
+    /*
+     * Funcion para solicitar una cancelación de compra, recibe el idPurchase
+     * activa a valor 1 el campo  solicitudCancelacion para que el administrador
+     * pueda cancelar la compra completa.
+     */
+    public function cancelrequest() {
+        $idPurchase = $this->input->post("idPurchase");
+
+        $response["responseStatus"] = false;
+
+        $this->load->model("PurchaseModel");
+        $updated = $this->PurchaseModel->cancelRequest($idPurchase);
+
+        if($updated) {
+            $response["responseStatus"] = "Ok";
+            $response["message"] = "Solicitud de cancelacion exitosa";
+        }
+        else {
+            $response["responseStatus"] = "FAIL";
+            $response["message"] = "No se pudo procesar la solicitud";
+        }
+        $this->output->set_content_type('application/json')->set_output(json_encode($response));
+    }
+
+}
---
+++ application/models/purchasemodel.php
@@ -0,0 +1,79 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class PurchaseModel extends CI_Model {
+
+    public function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+        //Connect to database
+        $this->load->database();
+    }
+
+
+    /*
+     * Funcion para obtener información de compra
+     */
+    public function getInfo($id) {
+
+        $this->db->select("id as idPurchase, pagoProcesado, referenciaPago as referencia, montoTotal, montoEnvio, idAddress, tipoPago");
+        $result = $this->db->get_where("Purchase",array("id"=>$id))->row();
+
+        //if ($result->idPurchase === null) return false;////
+        $this->db->select("entreCalles");
+        $idAdd= $result->idAddress;
+        $address = $this->db->get_where("Address",array("id"=>$idAdd))->row();
+
+        $purchase["idPurchase"]    = $result->idPurchase;
+        $purchase["pagoProcesado"] = $result->pagoProcesado;
+        $purchase["referencia"]    = $result->referencia;
+        $purchase["montoTotal"] = $result->montoTotal;
+        $purchase["montoEnvio"] = $result->montoEnvio;
+        $purchase["entreCalles"] = $address->entreCalles;
+        $purchase["idAdd"] = $idAdd;
+        $purchase["tipoPago"] = $result->tipoPago;
+        return $purchase;
+    }
+
+    // Funcion para cancelar una compra. Valida que el campo
+    // solicitudCancelacion = 1
+    // para eliminar el registro
+    public function cancel($id) {
+        /*$data = array(
+            "id" => $id,
+            "solicitudCancelacion" => 1
+        );*/
+        $this->db->where("id", $id);
+        $this->db->where("solicitudCancelacion", 1);
+        $this->db->delete("Purchase");
+
+        if($this->db->affected_rows() >1 ){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /* funcion para solicitar una cancelación de compra
+     * setea solicitudCancelacion = 1
+     * solo el administrador puede eliminar el registro
+     */
+    public function cancelRequest($id) {
+        $data = array(
+            "solicitudCancelacion"=>1
+        );
+
+        $this->db->where("id", $id);
+        $this->db->update("Purchase",$data);
+
+        if ($this->db->affected_rows() >0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+}
